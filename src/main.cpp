b1c5b2fd29e486c72bc5bf3348422c01c7634f89
Краснобаев Алексей <rakeliit@protonmail.com>
2025-11-25 15:21:44 +0200

    Firs framework Code

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include <TFT_eSPI.h>
+
+
+TFT_eSPI tft = TFT_eSPI(); 
+
+void setup() 
+{
+  tft.init();
+}
+
+void loop()
+{
+  
+} 
+
