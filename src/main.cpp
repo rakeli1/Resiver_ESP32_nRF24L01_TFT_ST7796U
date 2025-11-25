ac4b4b0b8626ea21e69e80e2144bca0728b26d42
Краснобаев Алексей <rakeliit@protonmail.com>
2025-11-25 23:10:32 +0200

    Написал функцию рисующую линии

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,27 @@
 
 TFT_eSPI tft = TFT_eSPI(); 
 
+void drawVline(int32_t x0, int32_t y0, int32_t visota, int32_t color, int repit)
+{
+  int rep = 0;
+  int32_t dubX = x0;
+  while(rep < repit)
+ {
+  tft.drawFastVLine(dubX, y0, visota, color);
+  dubX++;
+  rep++;
+ }
+}
+
 void setup() 
 {
   tft.init();
+  tft.setRotation(3);
+  tft.fillScreen(TFT_DARKGREY);
 }
 
 void loop()
 {
-  
+  drawVline(240, 0, 320, TFT_BLACK, 2);
 } 
 
