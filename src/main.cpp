ef74b12ec087123c1f92ea6bd1648c3e6c16d0f7
Краснобаев Алексей <rakeliit@protonmail.com>
2025-11-26 21:56:40 +0200

    Набросок кода титульной страницы на дисплее

--- src/main.cpp
+++ src/main.cpp
@@ -19,15 +19,36 @@ void drawVline(int32_t x0, int32_t y0, int32_t visota, int32_t color, int repit)
  }
 }
 
+void drawHLine(int32_t x0, int32_t y0, int32_t chirina, int32_t color, int repit)
+{
+  int rep = 0;
+  int32_t dubY = y0;
+  while(rep < repit)
+ {
+  tft.drawFastHLine(x0, dubY, chirina, color);
+  dubY++;
+  rep++;
+ }
+}
+
 void setup() 
 {
   tft.init();
-  tft.setRotation(3);
+  tft.setRotation(3); // левый верхний угол - 0 координат(x- вправо , y - вниз). контакты дисплея слева 
   tft.fillScreen(TFT_DARKGREY);
 }
 
-void loop()
+void loop() 
 {
-  drawVline(240, 0, 320, TFT_BLACK, 2);
+  drawVline(160, 0, 320, TFT_WHITE, 2);
+  drawVline(320, 220, 100, TFT_WHITE, 2);
+  drawVline(320, 70, 250, TFT_WHITE, 2);
+  drawHLine(0, 220, 480, TFT_WHITE, 2);
+  drawHLine(0, 70, 480, TFT_WHITE, 2);
+  drawHLine(0, 145, 160, TFT_WHITE, 2);
+  
+  tft.fillRect(0, 0, 20, 20, TFT_DARKGREEN);  // ориентир setRotation()
+  tft.drawRect(170, 5, 40, 13, TFT_WHITE ); // индикатор батареи
+  tft.fillCircle(240, 125, 20, TFT_YELLOW);
 } 
 
